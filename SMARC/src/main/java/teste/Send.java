e0a3144a6081dd9c0836ff8b652f19f87c5aabca
2022122760265 <2022122760265@col-7110-li03.cefetes.br>
2024-11-06 13:56:17 -0300

    Adição da tela de cadastro de torneio

---
+++ SMARC/src/main/java/teste/Send.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package teste;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author 2022122760265
+ */
+public class Send {
+    public static void main(String[] args) {
+        //Endereço:
+        String multiCastAddress = "224.0.0.1";
+        final int multiCastPort = 52684;
+        
+        try{
+            //Create Socket
+            System.out.println("Create socket on address " + multiCastAddress + " and port " + multiCastPort + ".");
+            InetAddress group = InetAddress.getByName(multiCastAddress);
+            MulticastSocket s = new MulticastSocket(multiCastPort);
+            s.joinGroup(group);
+            
+            //Prepare Data
+            String message = "Hello world!";
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oss = new ObjectOutputStream(baos);
+            oss.writeObject(message);
+            
+            byte[] data = baos.toByteArray();
+            
+            s.send(new DatagramPacket(data, data.length, group, multiCastPort));
+        } catch(UnknownHostException e){
+            System.out.println("UnknownHostException: "+e.getMessage());
+        } catch(IOException e){
+            System.out.println("IOException: "+e.getMessage());
+        }
+    }  
+}
